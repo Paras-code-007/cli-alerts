2e511368d41c72693674f6e36f97ef1552274a43
Paras Arora <parasarora@Parass-MacBook-Air.local>
2020-10-28 03:42:44 +0530

    added alerts to the module for user preferncing

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 // npx conduct for code-of-conduct.md file
 // npx licensed MIT to generate the license file
+const chalk= require('chalk')
+const logSymbols= require('log-symbols')
+
+const success= chalk.bold.green
+const info= chalk.bold.hex('#6937FF')
+const warning= chalk.bold.keyword('orange')
+const error= chalk.bold.red
+
+module.exports= (options)=>{
+
+    const defaultOptions= {
+        type: 'error',
+        msg: 'you forgot to pass the options'
+    }
+
+    const opts= {...defaultOptions, ...options}
+
+    const name= opts.name ? opts.name.toUpperCase() : opts.type.toUpperCase()
+
+    if(opts.type=== 'success'){
+        console.log(`\n${logSymbols.success}${success(` ${name}: `+ opts.msg)}\n`)
+    }
+    if(opts.type=== 'info'){
+        console.log(`\n${logSymbols.info}${info(` ${name}: `+opts.msg)}\n`)
+    }
+    if(opts.type=== 'warning'){
+        console.log(`\n${logSymbols.warning}${warning(` ${name}: `+opts.msg)}\n`)
+    }
+    if(opts.type=== 'error'){
+        console.log(`\n${logSymbols.error}${error(` ${name}: `+opts.msg)}\n`)
+    }
+
+
+}
--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-const clialert = require('./index.js')
+const alert = require('./index.js')
+
+alert({
+    type: "success",
+    msg: "hello world",
+    name: 'done'
+})
+alert({type: "error",msg: "hello world"})
+alert({type: "warning",msg: "hello world"})
+alert({type: "info",msg: "hello world"})
+alert({type: "info",msg: "hello world", name: "remember"})
+alert()
